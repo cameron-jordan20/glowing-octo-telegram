55296976e323b22f1b6078dfcfa309d06e5a9341
cameron-jordan20 <camjor.init@gmail.com>
2021-05-29 12:22:42 +0200

    first commit

---
+++ scraper.js
@@ -0,0 +1,15 @@
+const cheerio = require('cheerio')
+const request = require('request')
+
+let price = null;
+function scrapeSPX(){
+  request('https://www.marketwatch.com/investing/index/spx', (error, Response, html)=>{
+    if(!error && Response.statusCode ==200){
+      const $ = cheerio.load(html)
+      price = $('.intraday__price ').children('bg-quote').text()
+      console.log(price)
+    }
+  })
+}
+
+scrapeSPX()
