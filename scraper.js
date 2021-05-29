cc8f9840cb026bc661eaf4202eff850ceee4ac93
cameron-jordan20 <camjor.init@gmail.com>
2021-05-29 12:50:27 +0200

    linked index.html, fixed syntax errors in scraper.js

--- scraper.js
+++ scraper.js
@@ -1,15 +1,16 @@
-const cheerio = require('cheerio')
-const request = require('request')
-
+const cheerio = require('cheerio');
+const request = require('request');
 let price = null;
+
 function scrapeSPX(){
-  request('https://www.marketwatch.com/investing/index/spx', (error, Response, html)=>{
-    if(!error && Response.statusCode ==200){
-      const $ = cheerio.load(html)
-      price = $('.intraday__price ').children('bg-quote').text()
-      console.log(price)
+  request('https://www.marketwatch.com/investing/index/spx', function(error, Response, html){
+    if (!error && Response.statusCode ==200){
+      const $ = cheerio.load(html);
+      price = $('.intraday__price ').children('bg-quote').text();
+      console.log(price);
     }
-  })
+  });
 }
 
-scrapeSPX()
+scrapeSPX();
+// document.getElementById("insert").innerHTML = scrapeSPX();
