ff79905ea7d9fbdad9037e291a59e60bad949fc0
cameron-jordan20 <camjor.init@gmail.com>
2021-05-29 13:11:04 +0200

    added funtions for COMP and DJIA

--- scraper.js
+++ scraper.js
@@ -1,16 +1,43 @@
 const cheerio = require('cheerio');
 const request = require('request');
-let price = null;
 
+let spx = null;
 function scrapeSPX(){
-  request('https://www.marketwatch.com/investing/index/spx', function(error, Response, html){
+  request('http://www.marketwatch.com/investing/index/spx', function(error, Response, html){
     if (!error && Response.statusCode ==200){
       const $ = cheerio.load(html);
-      price = $('.intraday__price ').children('bg-quote').text();
-      console.log(price);
+      spx = $('.intraday__price ').children('.value').text();
+      console.log('S&P 500 ' + spx);
+    }
+  });
+}
+
+let comp = null;
+function scrapeCOMP(){
+  request('https://www.marketwatch.com/investing/index/comp', function(error, Response, html){
+    if (!error && Response.statusCode ==200){
+      const $ = cheerio.load(html);
+      comp = $('.intraday__price ').children('.value').text();
+      console.log('Nasdaq ' + comp);
+    }
+  });
+}
+
+let djia = null;
+function scrapeDJIA(){
+  request('https://www.marketwatch.com/investing/index/djia', function(error, Response, html){
+    if (!error && Response.statusCode ==200){
+      const $ = cheerio.load(html);
+      djia = $('.intraday__price ').children('.value').text();
+      console.log('Dow Jones Industrial Average ' + djia);
     }
   });
 }
 
 scrapeSPX();
-// document.getElementById("insert").innerHTML = scrapeSPX();
+scrapeCOMP();
+scrapeDJIA();
+
+document.getElementById("spx").innerHTML = scrapeSPX();
+document.getElementById("comp").innerHTML = scrapeCOMP();
+document.getElementById("djia").innerHTML = scrapeDJIA();
